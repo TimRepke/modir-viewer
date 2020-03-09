7a967e2b6ed151602d44c391108e4fa089d455ea
Tim Repke <tim@repke.eu>
2020-03-09 19:06:10 +0100

    add demo page

---
+++ vis/data/dataCleaner.py
@@ -0,0 +1,36 @@
+import json
+
+bad_words = ['com', 'enron', 'http', 'www', 'mail', 'cc', 'subject']
+
+def cell_sort_key(word):
+	return word[1]
+
+def clean(filename):
+	with open(filename, 'r') as f:
+		data = json.load(f)
+
+		words = data['wordgrid']['words']
+		words_clean = []
+
+		for cell in words:
+			cell_clean = [word for word in cell if word[0].lower() not in bad_words]
+			cell_clean.sort(reverse=True, key=cell_sort_key)
+			words_clean.append(cell_clean)
+
+		mails = data['mails']
+		mails_clean = []
+		for i, mail in enumerate(mails):
+			mail['id'] = i
+			mails_clean.append(mail)
+
+		data['mails'] = mails_clean
+
+		print(words_clean)
+
+		data['wordgrid']['words'] = words_clean
+
+		with open(filename + '.clean.json', 'w') as outfile:
+			json.dump(data, outfile)
+
+if __name__ == '__main__':
+	clean('data/data_15.json.res.web.json')
--- visualisation/data/dataCleaner.py
+++ visualisation/data/dataCleaner.py
@@ -2,35 +2,38 @@ import json
 
 bad_words = ['com', 'enron', 'http', 'www', 'mail', 'cc', 'subject']
 
+
 def cell_sort_key(word):
-	return word[1]
+    return word[1]
+
 
 def clean(filename):
-	with open(filename, 'r') as f:
-		data = json.load(f)
+    with open(filename, 'r') as f:
+        data = json.load(f)
 
-		words = data['wordgrid']['words']
-		words_clean = []
+        words = data['wordgrid']['words']
+        words_clean = []
 
-		for cell in words:
-			cell_clean = [word for word in cell if word[0].lower() not in bad_words]
-			cell_clean.sort(reverse=True, key=cell_sort_key)
-			words_clean.append(cell_clean)
+        for cell in words:
+            cell_clean = [word for word in cell if word[0].lower() not in bad_words]
+            cell_clean.sort(reverse=True, key=cell_sort_key)
+            words_clean.append(cell_clean)
 
-		mails = data['mails']
-		mails_clean = []
-		for i, mail in enumerate(mails):
-			mail['id'] = i
-			mails_clean.append(mail)
+        mails = data['mails']
+        mails_clean = []
+        for i, mail in enumerate(mails):
+            mail['id'] = i
+            mails_clean.append(mail)
 
-		data['mails'] = mails_clean
+        data['mails'] = mails_clean
 
-		print(words_clean)
+        print(words_clean)
 
-		data['wordgrid']['words'] = words_clean
+        data['wordgrid']['words'] = words_clean
 
-		with open(filename + '.clean.json', 'w') as outfile:
-			json.dump(data, outfile)
+        with open(filename + '.clean.json', 'w') as outfile:
+            json.dump(data, outfile)
+
 
 if __name__ == '__main__':
-	clean('data/data_15.json.res.web.json')
+    clean('data/data_15.json.res.web.json')
